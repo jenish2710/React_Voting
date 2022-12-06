542b6101fe5cedc55eef334da478792c65fd991c
jenta777 <101331881+jenta777@users.noreply.github.com>
2022-12-06 12:05:13 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+//👇🏻 React Router configuration & routes
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Photos from "./components/Photos";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import UploadPhoto from "./components/UploadPhoto";
+import MyPhotos from "./components/MyPhotos";
+import SharePhoto from "./components/SharePhoto";
+//👇🏻 React Toastify configuration
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+//👇🏻 Websockets configuration
+import { io } from "socket.io-client";
+
+const App = () => {
+    const socket = io.connect("http://localhost:4000");
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login socket={socket} />} />
+                    <Route path='/register' element={<Register socket={socket} />} />
+                    <Route path='/photos' element={<Photos socket={socket} />} />
+                    <Route
+                        path='/photo/upload'
+                        element={<UploadPhoto socket={socket} />}
+                    />
+                    <Route path='/user/photos' element={<MyPhotos socket={socket} />} />
+                    <Route path='/share/:user' element={<SharePhoto socket={socket} />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer />
+        </>
+    );
+};
+
+export default App;
